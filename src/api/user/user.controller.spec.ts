5481ddeba5e3b59800cf43ebe000250744eeb9e8
Durmuş <demirtasdurmus@gmail.com>
2023-03-14 15:41:27 +0300

    ADD unit tests for user service and controller

--- src/api/user/user.controller.spec.ts
+++ src/api/user/user.controller.spec.ts
@@ -1,12 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { UserService } from './user.service';
 import { UserController } from './user.controller';
 
 describe('UserController', () => {
   let controller: UserController;
+  let fakeUserService: Partial<UserService>;
 
   beforeEach(async () => {
+    fakeUserService = {
+      async find(email: string) {
+        return [
+          {
+            id: '1',
+            email: email,
+            password: 'asd.asd',
+          },
+        ];
+      },
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [
+        {
+          provide: UserService,
+          useValue: fakeUserService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
@@ -14,5 +33,19 @@ describe('UserController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('Login', () => {
+    it('should update session object and return success', async () => {
+      const res = await controller.getAll('email@domain.com');
+
+      expect(res).toEqual([
+        {
+          id: '1',
+          email: 'email@domain.com',
+          password: 'asd.asd',
+        },
+      ]);
+    });
   });
 });
