4eb2d20ec72a530e7692c763ebe86f71732dee64
Durmuş <demirtasdurmus@gmail.com>
2023-03-16 06:14:13 +0300

    ADD session secret to .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -39,11 +39,13 @@ const cookieSession = require('cookie-session');
   ],
 })
 export class AppModule {
+  constructor(private readonly configService: ConfigService) {}
+
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(
         cookieSession({
-          keys: ['secret'],
+          keys: [this.configService.get('COOKIE_KEY')],
         }),
       )
       .forRoutes('*');
