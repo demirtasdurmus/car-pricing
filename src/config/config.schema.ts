32244ebb277635fbfb01f30a3222cb3e433295f5
Durmuş <demirtasdurmus@gmail.com>
2023-03-20 06:29:29 +0300

    IMPROVE config schema

--- src/config/config.schema.ts
+++ src/config/config.schema.ts
@@ -5,9 +5,9 @@ export const configValidationSchema = Joi.object<IConfig>({
   APP_PORT: Joi.number().required(),
   DB_TYPE: Joi.alt(['postgres', 'sqlite']).required(),
   DB_HOST: Joi.string().required(),
-  DB_PORT: Joi.string().required(),
+  DB_PORT: Joi.number().required(),
   DB_USERNAME: Joi.string().required(),
   DB_PASS: Joi.string().required(),
   DB_SYNC: Joi.boolean().required(),
-  COOKIE_KEY: Joi.string().required(),
+  COOKIE_KEY: Joi.string().required().min(32),
 });
